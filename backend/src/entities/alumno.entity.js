e27b31c88a5f3c303dec0f3ca34af1a8711a6598
Gabo288 <elgabrieljeres@gmail.com>
2025-11-12 12:11:05 -0300

    Add files via upload

---
+++ backend/src/entities/alumno.entity.js
@@ -0,0 +1,43 @@
+import { EntitySchema } from "typeorm";
+
+export const Alumno = new EntitySchema({
+    name: "Alumno",
+    tableName: "alumnos",
+    columns: {
+        id: {
+            primary: true,
+            type: "int",
+            generated: "increment",
+        },
+        rut: {
+            primary: true,
+            type: "varchar",
+            length: 12,
+            unique: true,
+            format: "XXXXXXXX-X",
+            nullable: false,
+
+        },
+        carrera: {
+            type: "varchar",
+            length: 255,
+            nullable: false,
+
+        },
+        creditos_aprovados: {
+            type: "int",
+            nullable: false,
+        },
+        create_at: {
+            type: "timestamp",
+            createDate: true,
+            nullable: false,
+        },
+        update_at: {
+            type: "timestamp",
+            updateDate: true,
+            nullable: false,
+        },
+    },
+
+});
