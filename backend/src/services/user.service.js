662268b64942b141fdd6b73f0fa2887991eda32e
Gabo288 <elgabrieljeres@gmail.com>
2025-11-12 11:53:00 -0300

    modified:   backend/src/routes/index.routes.js modified:   backend/src/services/user.service.js

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-import { AppDataSource } from "../config/configDB.js";
-import { User } from "../entities/user.entity.js";
-import bcrypt from "bcrypt";
-
-const userRepository = AppDataSource.getRepository(User);
-
-export async function createUser(data) {
-    const hashedPassword = await bcrypt.hash(data.password, 10);
-
-    const newUser = userRepository.create({
-        rut: data.rut,
-        nombre: data.nombre,
-        apellidos: data.apellidos,
-        email: data.email,
-        password: hashedPassword,
-        cargo: data.cargo,
-    });
-
-    return await userRepository.save(newUser);
-}
-
-export async function findUserByEmail(email) {
-    return await userRepository.findOneBy({ email });
-}
-export async function findUserByRut(rut) {
-    return await userRepository.findOneBy({ rut });
-}
-export async function updateUser(rut, data) {
-    try {
-        const user = await userRepository.findOneBy({ rut });
-        await userRepository.update(user.id, user);
-    } catch (error) {
-        throw new Error("Usuario no encontrado", error);
-    }
-}
-export async function deleteUser(rut) {
-    try {
-        const user = await userRepository.findOneBy({ rut });
-        await userRepository.remove(user);
-    } catch (error) {
-        throw new Error("Usuario no encontrado", error);
-    }
-    return true;
-}
-export async function getAllUsers() {
-    return await userRepository.find();
-}
-=======
 import { AppDataSource } from "../config/configDB.js";
 import { User } from "../entities/user.entity.js";
 import bcrypt from "bcrypt";
@@ -95,4 +46,3 @@ export async function deleteUser(rut) {
 export async function getAllUsers() {
     return await userRepository.find();
 }
->>>>>>> 8257cfc692c41b6448c091f710a4a00414d543b8
