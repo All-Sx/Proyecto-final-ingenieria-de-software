a3024a8be1e992617e72f36b7160f97c5ce78381
Gabo288 <elgabrieljeres@gmail.com>
2025-11-12 12:12:39 -0300

    Add files via upload

---
+++ backend/src/services/alumno.service.js
@@ -0,0 +1,40 @@
+import { AppDataSource } from "../config/configDB.js";
+import { Alumno } from "../entities/alumno.entity.js";
+
+const alumnoRepository = AppDataSource.getRepository(Alumno);
+
+export async function createAlumno(data) {
+
+    const newAlumno = alumnoRepository.create({
+        rut: data.rut,
+        carrera: data.carrera,
+        ingreso: data.ingreso,
+        creditos_aprovados: data.creditos_aprovados,
+
+    });
+
+    return await alumnoRepository.save(newAlumno);
+}
+export async function findAlumnoByRut(rut) {
+    return await alumnoRepository.findOneBy({ rut });
+}
+export async function updateAlumno(rut, data) {
+    try {
+        const alumno = await alumnoRepository.findOneBy({ rut });
+        await alumnoRepository.update(alumno.rut, alumno);
+    } catch (error) {
+        throw new Error("Alumno no encontrado", error);
+    }
+}
+export async function deleteAlumno(rut) {
+    try {
+        const alumno = await alumnoRepository.findOneBy({ rut });
+        await alumnoRepository.remove(alumno);
+    } catch (error) {
+        throw new Error("Alumno no encontrado", error);
+    }
+    return true;
+}
+export async function getAllAlumnos() {
+    return await alumnoRepository.find();
+}
