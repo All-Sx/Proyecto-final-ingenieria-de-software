7ba798232ca878dabfa56b6956d53116bf93cffe
Alan Fica <alan.fica2101@alumnos.ubiobio.cl>
2025-12-25 23:25:00 -0300

    Se agrego las funcionalidades de ver los electivos siendo jefe de carrera y modificarlos
    Se agrego la funcion getElectivosService y updateElectivoService para realizar las funcionalidades

--- backend/src/routes/electivo.routes.js
+++ backend/src/routes/electivo.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createElectivo } from "../controllers/electivo.controller.js";
+import { createElectivo, getElectivos, updateElectivo } from "../controllers/electivo.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 import { isAdmin } from "../middleware/authorization.middleware.js";
 
@@ -10,6 +10,18 @@ router.post("/",
     authMiddleware,               // 1. ¿Tiene token válido?
     isAdmin(["Jefe de Carrera", "Profesor"]), // 2. ¿Es Jefe de Carrera?
     createElectivo                // 3. Crear el electivo
+);
+
+router.get("/", 
+    authMiddleware,
+    isAdmin(["Jefe de Carrera"]), // Restringido solo al Jefe
+    getElectivos
+);
+
+router.put("/:id", 
+    authMiddleware, 
+    isAdmin(["Jefe de Carrera"]), // Solo el Jefe puede editar
+    updateElectivo
 );
 
 export default router;
