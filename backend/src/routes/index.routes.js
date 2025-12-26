2a2ed2a3d1eb1025ab1819e72d034a515d32941b
Manuel Cofré <manuel.cofre2101@alumnos.ubiobio.cl>
2025-12-26 15:58:02 -0300

    cambios en la fecha e inscripcion

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -3,7 +3,7 @@ import authRoutes from "./auth.routes.js";
 import electivoRoutes from "./electivo.routes.js";
 import periodoRoutes from "./periodo.routes.js";
 import userRoutes from "./usuario.routes.js";
-
+import inscripcionRoutes from "./inscripcion.routes.js";
 
 export function routerApi(app) {
     const router = Router();
@@ -16,4 +16,5 @@ export function routerApi(app) {
     router.use("/electivos", electivoRoutes); // /api/electivos
     router.use("/periodos", periodoRoutes); // /api/periodos
     router.use("/usuarios", userRoutes);     // /api/usuarios
+    router.use("/inscripciones", inscripcionRoutes); // /api/inscripciones
 }
