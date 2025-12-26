510ed2718501361f730d56c0ae1a6bdff71bd78c
Manuel Cofré <manuel.cofre2101@alumnos.ubiobio.cl>
2025-12-26 20:12:58 -0300

    limpieza

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -121,15 +121,7 @@ export const register = async (req, res) => {
       nombre_completo,
       email,
       password_hash: passwordHash,
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
       rol: rolEntity,  // Ahora usa el rol que encontramos
-=======
-      rol: rol,  
->>>>>>> Stashed changes
-=======
-      rol: rol,  
->>>>>>> Stashed changes
       activo: true
     });
 
